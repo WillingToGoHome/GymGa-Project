255a97d465cfafb7d23fa3a381a81499896b6db5
김진용 <jyjy0509@naver.com>
2024-05-02 00:01:49 +0900

    commit

---
+++ src/main/java/com/willingtogohome/gym/controller/PassController.java
@@ -0,0 +1,4 @@
+package com.willingtogohome.gym.controller;
+
+public class PassController {
+}
---
+++ src/main/java/com/willingtogohome/gymga/GymGaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.willingtogohome.gymga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymGaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymGaProjectApplication.class, args);
+    }
+
+}
