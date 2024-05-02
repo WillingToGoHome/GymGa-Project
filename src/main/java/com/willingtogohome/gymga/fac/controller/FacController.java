250f753b57ebaaf87b9f569fed0cfe5cf1528d14
LEEMINKUK <158013587+LEEMINKUK@users.noreply.github.com>
2024-05-02 22:20:24 +0900

    add graph

--- src/main/java/com/willingtogohome/gymga/fac/controller/FacController.java
+++ src/main/java/com/willingtogohome/gymga/fac/controller/FacController.java
@@ -1,31 +1,10 @@
 package com.willingtogohome.gymga.fac.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/facility")
 public class FacController {
 
-    private final FacService facService;
-
-    public FacController(FacService facService) {
-        this.facService=facService;
-    }
-
-    @GetMapping("/select")
-    public String findFacList(Model model) {
-        List<FacDTO> facList=facService.findAllFac();
-
-        for(FacDTO facs:facList) {
-            System.out.println("facs = " + facs);
-        }
-        model.addAttribute("facList",facList);
-
-        return "facility/select";
-    }
 }
--- src/main/java/com/willingtogohome/gymga/sale/controller/SaleController.java
+++ src/main/java/com/willingtogohome/gymga/sale/controller/SaleController.java
@@ -1,4 +1,22 @@
 package com.willingtogohome.gymga.sale.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class SaleController {
+
+    @GetMapping("/sale/main")
+    public String saleMain(){
+
+        return "sale/main";
+    }
+
+
+
+
+
+
 }
