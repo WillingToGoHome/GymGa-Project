df4c5cd4398f136420d376c21cbddf4be43f388d
sunskwon <sunskwon@gmail.com>
2024-05-04 22:11:33 +0900

    [feature][pass]candidate_kwon

--- src/main/java/com/willingtogohome/gymga/pass/controller/PassController.java
+++ src/main/java/com/willingtogohome/gymga/pass/controller/PassController.java
@@ -1,4 +1,16 @@
 package com.willingtogohome.gymga.pass.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pass")
 public class PassController {
+
+    @GetMapping(value={"/", "/main"})
+    public String passMain() {
+
+        return "/pass/main";
+    }
 }
