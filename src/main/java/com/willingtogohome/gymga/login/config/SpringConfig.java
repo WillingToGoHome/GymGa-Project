48361210d8b5a49d65a5b944b3297e72e813884c
sunskwon <sunskwon@gmail.com>
2024-05-10 18:18:35 +0900

    commit

--- src/main/java/com/willingtogohome/gymga/login/config/SpringConfig.java
+++ src/main/java/com/willingtogohome/gymga/login/config/SpringConfig.java
@@ -1,9 +1,9 @@
-package com.willingtogohome.gymga.login.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.willingtogohome")
-public class SpringConfig {
-}
+//package com.willingtogohome.gymga.login.config;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@ComponentScan(basePackages = "com.willingtogohome")
+//public class SpringConfig {
+//}
