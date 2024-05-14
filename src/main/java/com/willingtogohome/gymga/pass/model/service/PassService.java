84dc42adef2d380bd4ca9dbbb0f7d0b192e9080e
sunskwon <sunskwon@gmail.com>
2024-05-14 17:55:12 +0900

    [FEATURE][PASS]be-regist

--- src/main/java/com/willingtogohome/gymga/pass/model/service/PassService.java
+++ src/main/java/com/willingtogohome/gymga/pass/model/service/PassService.java
@@ -1,9 +1,10 @@
 package com.willingtogohome.gymga.pass.model.service;
 
 import com.willingtogohome.gymga.pass.model.dao.PassMapper;
-import com.willingtogohome.gymga.pass.model.dto.UserDTO;
+import com.willingtogohome.gymga.pass.model.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +22,24 @@ public class PassService {
     public List<UserDTO> selectAllEmp() {
 
         return passMapper.selectAllEmp();
+    }
+
+    public List<UserDTO> selectAllMember(SearchCriteria searchCriteria) {
+
+        return passMapper.selectAllMember(searchCriteria);
+    }
+
+    public int findMaxCode() {
+
+        return passMapper.findMaxCode();
+    }
+
+    @Transactional
+    public void registNewPass(PassDTO passDTO, SaleDTO saleDTO, ValidateDTO validateDTO) {
+
+        passMapper.registNewPassDTO(passDTO);
+        passMapper.registNewSaleDTO(saleDTO);
+        passMapper.registNewValidateDTO(validateDTO);
+
     }
 }
